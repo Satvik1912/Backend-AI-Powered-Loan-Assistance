9f6456821a6306289eedcf596df0c3374f9dd893
Praadnya <hpraadnya@gmail.com>
2025-02-25 10:38:22 +0530

    GetLoan and GetAllLoans

---
+++ src/main/java/com/cars24/ai_loan_assistance/exceptions/LoanServiceException.java
@@ -0,0 +1,8 @@
+package com.cars24.ai_loan_assistance.exceptions;
+
+public class LoanServiceException extends RuntimeException{
+    public LoanServiceException(String message){
+        super(message);
+    }
+
+}
